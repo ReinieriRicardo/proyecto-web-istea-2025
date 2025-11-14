f8923ac31b017c3335972294e3b2f225755e6991
Reinieri Ricardo <ricardo.reinieri@istea.com.ar>
2025-11-14 16:34:23 -0300

    refactor: move Airtable configuration to separate config file

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-// config.js
 const AIRTABLE_CONFIG = {
-    TOKEN: 'pat8tLFZBFuxZGFpv.936b0a3bb0fb9a97d7b04e2faa8d46d3c1af8269784a5170f5cf34a06b9206f2'',
+    TOKEN: 'pat8tLFZBFuxZGFpv.936b0a3bb0fb9a97d7b04e2faa8d46d3c1af8269784a5170f5cf34a06b9206f2',
     BASE_ID: 'appQAusygx01OOuTl',
     TABLE_NAME: 'Productos'
 };
